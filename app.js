09271f8062b85afa3ff11aec5a47978e6d9e328a
Hemant D <hemant132us@gmail.com>
2023-01-23 15:53:14 +0530

    new update

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 var fs = require('fs');
+var Git = require("nodegit");
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     var htmlContent = '<html>Whatever</html>';
-    fs.writeFile('./my-page.html', htmlContent, (error) => { /* handle error */ });
+    var name = 'newbot';
+    fs.writeFile(`./${name}.html`, htmlContent, (error) => { /* handle error */ });
     res.status(200).send({
-      message: 'Hello from AI!'
+      message: `Hello from ${name}!`
+
+      
     })
     
 })
